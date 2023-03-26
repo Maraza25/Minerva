bb0cdb85f008b5e7c167464892341b099a6b2eb1
Maraza25 <agirmanberat668@gmail.com>
2023-03-26 18:05:33 +0300

    acılır menü ve animasyonu eklendi

--- src/Components/Topic/Topic.js
+++ src/Components/Topic/Topic.js
@@ -1,54 +1,102 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./Topic.css";
 import Research from "../Research/Research";
 
-export default class Topic extends Component {
-  render() {
-    return (
-      <div className="topic-container">
-        <Research
-          image={true} // ya da false
-          title="Bir Araştasdadasd dasdadadsasdırma Başlığı"
-          description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
-          imageUrl="https://picsum.photos/100"
-        />
-        <Research
-          image={false} // ya da false
-          title="Bir Araştırma Başlığı"
-          description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
-          imageUrl="https://picsum.photos/200"
-        />{" "}
-        <Research
-          image={false} // ya da false
-          title="Bir Araştırma Başlığı"
-          description="Bu bir araştırma hakkında bir açıklama."
-          imageUrl="https://picsum.photos/200"
-        />{" "}
-        <Research
-          image={false} // ya da false
-          title="Bir Araştırma Başlığı"
-          description="Bu bir araştırma hakkında bir açıklama."
-          imageUrl="https://picsum.photos/200"
-        />{" "}
-        <Research
-          image={false} // ya da false
-          title="Bir Araştırma Başlığı"
-          description="Bu bir araştırma hakkında bir açıklama."
-          imageUrl="https://picsum.photos/200"
-        />{" "}
-        <Research
-          image={false} // ya da false
-          title="Bir Araştırma Başlığı"
-          description="Bu bir araştırma hakkında bir açıklama."
-          imageUrl="https://picsum.photos/200"
-        />{" "}
-        <Research
-          image={false} // ya da false
-          title="Bir Araştırma Başlığı"
-          description="Bu bir araştırma hakkında bir açıklama."
-          imageUrl="https://picsum.photos/200"
-        />
+export default function Topic() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+  };
+
+  const handleClose = () => {
+    setIsClicked(false);
+  };
+
+  return (
+    <div className="topic-container">
+      <Research
+        image={true} // ya da false
+        title="Bir Araştasdadasd dasdadadsasdırma Başlığı"
+        description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
+        imageUrl="https://picsum.photos/100"
+      />
+      <Research
+        image={false} // ya da false
+        title="Bir Araştırma Başlığı"
+        description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
+        imageUrl="https://picsum.photos/200"
+      /> <Research
+      image={true} // ya da false
+      title="Bir Araştasdadasd dasdadadsasdırma Başlığı"
+      description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
+      imageUrl="https://picsum.photos/100"
+    />
+    <Research
+      image={false} // ya da false
+      title="Bir Araştırma Başlığı"
+      description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
+      imageUrl="https://picsum.photos/200"
+    /> <Research
+    image={true} // ya da false
+    title="Bir Araştasdadasd dasdadadsasdırma Başlığı"
+    description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
+    imageUrl="https://picsum.photos/100"
+  />
+  <Research
+    image={false} // ya da false
+    title="Bir Araştırma Başlığı"
+    description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
+    imageUrl="https://picsum.photos/200"
+  /> <Research
+  image={true} // ya da false
+  title="Bir Araştasdadasd dasdadadsasdırma Başlığı"
+  description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
+  imageUrl="https://picsum.photos/100"
+/>
+<Research
+  image={false} // ya da false
+  title="Bir Araştırma Başlığı"
+  description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
+  imageUrl="https://picsum.photos/200"
+/> <Research
+        image={true} // ya da false
+        title="Bir Araştasdadasd dasdadadsasdırma Başlığı"
+        description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
+        imageUrl="https://picsum.photos/100"
+      />
+      <Research
+        image={false} // ya da false
+        title="Bir Araştırma Başlığı"
+        description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
+        imageUrl="https://picsum.photos/200"
+      /> <Research
+      image={true} // ya da false
+      title="Bir Araştasdadasd dasdadadsasdırma Başlığı"
+      description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
+      imageUrl="https://picsum.photos/100"
+    />
+    <Research
+      image={false} // ya da false
+      title="Bir Araştırma Başlığı"
+      description="Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama.Bu bir araştırma hakkında bir açıklama."
+      imageUrl="https://picsum.photos/200"
+    />
+        {isClicked && (
+          <div className="animation">
+           <div className="add-research-container">
+          <div className="add-research">
+          </div>
+        </div>
+          </div>
+        )}
+        
+        <div className="button-container">
+          <button onClick={handleClick}>Animasyonlu Div'i Göster</button>
+        </div>
       </div>
-    );
-  }
+  );
 }
+ {/* <button className="close-button" onClick={handleClose}>
+              X
+            </button> */}
